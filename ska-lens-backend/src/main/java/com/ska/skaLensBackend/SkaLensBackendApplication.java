a1cccfd1349f357df4ac32446cf291b5fec26404
sAkuraOfficial <mchaohao159@outlook.com>
2026-03-30 13:50:52 +0800

    init project

---
+++ ska-lens-backend/src/main/java/com/ska/skaLensBackend/SkaLensBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ska.skaLensBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkaLensBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkaLensBackendApplication.class, args);
+    }
+
+}
---
+++ ska-lens-backend/src/test/java/com/ska/skaLensBackend/SkaLensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ska.skaLensBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkaLensBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
